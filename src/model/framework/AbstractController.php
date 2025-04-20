32e32e134019e40f0f9289158dc064d3fdcc9dc7
chau-nm <chaunm.146.2001@gmail.com>
2025-04-20 14:48:37 +0700

    chore: insert redirect

--- src/model/framework/AbstractController.php
+++ src/model/framework/AbstractController.php
@@ -2,6 +2,7 @@
 
 namespace app\model\framework;
 
+use app\model\response\RedirectResponse;
 use app\model\response\ViewResponse;
 
 abstract class AbstractController
@@ -9,5 +10,11 @@ abstract class AbstractController
     protected function render(string $view, array $data = []): ViewResponse
     {
         return new ViewResponse($view, $data);
+    }
+
+    protected function redirect(string $url): void
+    {
+        $driver = new RedirectResponse($url);
+        $driver->redirect();
     }
 }
